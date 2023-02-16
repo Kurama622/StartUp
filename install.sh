87496d86772300fffcd35473547e4cb948269ce4
StubbornVegeta <2273893094@qq.com>
2023-02-16 21:10:41 +0800

    compile: add xmake compile

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-if [ ! -d "build" ]; then
-    mkdir build
+if which xmake >/dev/null 2>&1; then
+    xmake build
+    sudo xmake install
+else
+    if [ ! -d "build" ]; then
+        mkdir build
+    fi
+    cd build
+    if [ ! -f "Makefile" ]; then
+        cmake ..
+    fi
+    make
+    sudo cp StartUp /usr/local/bin/ 
 fi
-cd build
-if [ ! -f "Makefile" ]; then
-    cmake ..
-fi
-make
-sudo cp StartUp /usr/local/bin/ 
 
